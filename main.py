d188a74caa3452eaac93ecd491adf187f9155b80
Laurent Blondy <laurent.blondy1@gmail.com>
2020-02-25 08:07:32 +0100

    1

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import numpy as np
 from math import sqrt
 from utils import chrono
-<<<<<<< HEAD
 from primes import get_primes, get_isPrime
 from itertools import product
-=======
-
-
-@chrono
-def countTo(bound):
-    c = 0
-    for i in range(bound):
-        c += 1
-    return c
-
-
-countTo(1000000)
-
-//test
->>>>>>> 8e5b859236e4357c7249a0db30c6ab359bdaf195
